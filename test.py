036f5b65bfce30963bab5e3427ef8c1c0d6243fd
ap3j <107736366+ap3j@users.noreply.github.com>
2024-04-10 13:34:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -21,38 +21,38 @@ def render_cybersalaries():
     st.title("About Page")
     st.write("This is the about page content.")
   
-    def main():
+def main():
     # URL of the raw CSV file on GitHub
-        csv_url = 'https://raw.githubusercontent.com/ap3j/streamlit123/main/salaries_cyber.csv'
+    csv_url = 'https://raw.githubusercontent.com/ap3j/streamlit123/main/salaries_cyber.csv'
 
     # Read the CSV file into a DataFrame
-        df = pd.read_csv(csv_url, encoding='utf-8',na_values=['NA', '']) 
+    df = pd.read_csv(csv_url, encoding='utf-8',na_values=['NA', '']) 
 
         # Display the DataFrame as a data table
-        st.write(df)
+     st.write(df)
 
-        if __name__ == "__main__":
-            main()
+    if __name__ == "__main__":
+        main()
 
-    def bar():
-        st.title('Salary By Experience')
+def bar():
+    st.title('Salary By Experience')
     
     # Sample data
-        data = {
-            'Category': ['EN', 'MI', 'SN', 'EX'],
-            'Value': [63579, 103337, 144560, 200706]
-        }
+    data = {
+        'Category': ['EN', 'MI', 'SN', 'EX'],
+        'Value': [63579, 103337, 144560, 200706]
+    }
 
     # Create DataFrame
-        df_topsal = pd.DataFrame(data)
+    df_topsal = pd.DataFrame(data)
 
     # Create Seaborn bar plot
-        fig, ax = plt.subplots()
-        sns.barplot(x='Category', y='Value', data=df_topsal, ax=ax)
-        ax.set_title('Bar Plot')
+    fig, ax = plt.subplots()
+    sns.barplot(x='Category', y='Value', data=df_topsal, ax=ax)
+    ax.set_title('Bar Plot')
 
     # Display the plot in Streamlit
-        st.pyplot(fig)
+     st.pyplot(fig)
 
     if __name__ == "__main__":
         bar()
