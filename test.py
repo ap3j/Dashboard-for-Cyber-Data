7b09c5bb41cd58177f99d9444c49467699d8afdf
ap3j <107736366+ap3j@users.noreply.github.com>
2024-04-10 13:11:57 -0700

    Update test.py

--- test.py
+++ test.py
@@ -16,9 +16,60 @@ def render_page(page):
     if page == "Home":
         st.title("Welcome to the Home Page!")
         st.write("I am a data science student at Middle Tennessee State University where I specialzie in the cyber security domain. I hope you get some useful insights fromy my dashboards and visualizations!")
-    elif page == "About":
+    elif page == "Cyber Salaries":
         st.title("About Page")
         st.write("This is the about page content.")
+        st.code("""
+  if __name__ == "__main__":
+    main()
+
+def bar():
+    st.title('Salary By Experience')
+    
+    # Sample data
+    data = {
+        'Category': ['EN', 'MI', 'SN', 'EX'],
+        'Value': [63579, 103337, 144560, 200706]
+    }
+
+    # Create DataFrame
+    df_topsal = pd.DataFrame(data)
+
+    # Create Seaborn bar plot
+    fig, ax = plt.subplots()
+    sns.barplot(x='Category', y='Value', data=df_topsal, ax=ax)
+    ax.set_title('Bar Plot')
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+if __name__ == "__main__":
+    bar()
+
+def bar2():
+    st.title('Salary By Job Title Top 5')
+    
+    # Sample data
+    data = {
+        'Value': [315000, 295000, 260000, 237000,235540],
+        'Job Title': ['Application Security Architect', 'Staff Security Engineer', 'Threat Intellgience Response Analyst', 
+                     'Principal Applicaiton Security Engineer','Software Security Engineer']
+    }
+
+    # Create DataFrame
+    df_topsal15 = pd.DataFrame(data)
+
+    # Create Seaborn bar plot
+    fig, ax = plt.subplots()
+    sns.barplot(x='Value', y='Job Title', data=df_topsal15, ax=ax)
+    ax.set_title('Bar Plot')
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+if __name__ == "__main__":
+    bar2()
+    """)
     elif page == "Contact":
         st.title("Contact Page")
         st.write("This is the contact page content.")
