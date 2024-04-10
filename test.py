e6a3b9b1b7a75d415dad8e0f1a12fb12d1cf8194
ap3j <107736366+ap3j@users.noreply.github.com>
2024-04-09 22:31:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,6 @@ I hope you get some useful insights fromy my dashboards and visualizations!
 
 csv_url = 'https://github.com/ap3j/streamlit123/blob/main/salaries_cyber.csv'
 
-data = pd.read_csv(csv_url)
+data = pd.read_csv(csv_url, encoding='utf-8')
 
 print(data)
