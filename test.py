143b758ac78a5f0277cfc9bdded688649c30ca85
ap3j <107736366+ap3j@users.noreply.github.com>
2024-04-09 22:08:39 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to my Streamlit page!
+I am a data science student at Middle Tennessee State University where I specialzie in the cyber security domain. 
+I hope you get some useful insights fromy my dashboards and visualizations!
+
+"""
+data = pd.read_csv_("salaries_cyber.csv")
